8f773980af018ad3c675fc096a73e501ed4c8429
Jim Posen <jim.posen@gmail.com>
2021-09-07 12:15:04 -0400

    createDriver request validation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import Koa from 'koa';
+import Koa, {ExtendableContext} from 'koa';
 import Router from '@koa/router';
+import bodyParser from 'koa-bodyparser';
+
+import {listDrivers, createDriver} from './routes/drivers';
+
+async function checkJsonContentType(ctx: ExtendableContext, next: Koa.Next) {
+    /// The === false is because null indicates no request body
+    if (ctx.is('application/json') === false) {
+        ctx.throw(415, 'API only accepts JSON requests');
+    }
+    await next();
+}
 
 function main() {
     const app = new Koa();
     const router = new Router();
 
-    router.get('/', async (ctx, next: Koa.Next) => {
-        ctx.body = { message: 'Hello world!' };
-        await next();
-    });
+    router
+        .prefix('/api')
+        .use(checkJsonContentType)
+        .use(bodyParser())
+        .get('/', async (ctx, next: Koa.Next) => {
+            ctx.body = { message: 'Hello world!' };
+            await next();
+        })
+        .get('/drivers', listDrivers)
+        .post('/drivers', createDriver);
 
     app
         .use(router.routes())
---
+++ src/routes/drivers.ts
@@ -0,0 +1,33 @@
+import Koa, {ExtendableContext} from 'koa';
+import Joi from 'joi';
+
+export async function listDrivers(ctx: ExtendableContext, next: Koa.Next) {
+    ctx.body = [];
+    await next();
+}
+
+export async function createDriver(ctx: ExtendableContext, next: Koa.Next) {
+    const requestSchema = Joi.object({
+        name: Joi.string()
+            .required(),
+        ipAddress: Joi.string()
+            .ip({ version: ['ipv4', 'ipv6'], cidr: 'forbidden' })
+            .required(),
+    });
+
+    const { value: body, error } = requestSchema.validate(ctx.request.body);
+    if (error) {
+        ctx.status = 422;
+        ctx.body = error.details;
+        return await next();
+    }
+    const name: string = body.name;
+    const ipAddress: string = body.ipAddress;
+
+    ctx.status = 201;
+    ctx.body = {
+        name,
+        ipAddress,
+    };
+    await next();
+}
