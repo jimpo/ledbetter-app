ac5e9813050d4670f3a26115226ed5bad3a062f3
Jim Posen <jim.posen@gmail.com>
2021-09-07 10:29:07 -0400

    Koa server with router

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world')
+import Koa from 'koa';
+import Router from '@koa/router';
+
+function main() {
+    const app = new Koa();
+    const router = new Router();
+
+    router.get('/', async (ctx, next: Koa.Next) => {
+        ctx.body = { message: 'Hello world!' };
+        await next();
+    });
+
+    app
+        .use(router.routes())
+        .use(router.allowedMethods());
+
+    app.listen(3000);
+}
+
+main()
