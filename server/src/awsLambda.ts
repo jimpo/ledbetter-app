5b2937c61a62f553a29753bdf1df9eb90f105800
Jim Posen <jim.posen@gmail.com>
2022-10-13 16:35:23 -0400

    Lambda handler for migrations.

--- server/src/awsLambda.ts
+++ server/src/awsLambda.ts
@@ -1,7 +1,16 @@
 import serverless from 'serverless-http';
 
 import app from './app.js';
+import {db} from './db.js';
 
 const BINARY_CONTENT_TYPES = ['font/*', 'image/*'];
+const MIGRATIONS_DIRECTORY = './dist/migrations';
 
 export const handler = serverless(app, {binary: BINARY_CONTENT_TYPES});
+
+export async function migrate(): Promise<void> {
+	const config = {directory: MIGRATIONS_DIRECTORY};
+	await db.migrate.latest(config);
+	const migrationVersion = await db.migrate.currentVersion(config);
+	console.log(`Migrations complete, current version: ${migrationVersion}`);
+}
