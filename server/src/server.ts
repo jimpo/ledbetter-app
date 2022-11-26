57eafb550efebf45ab9bc9414446ac162a335abe
Jim Posen <jim.posen@gmail.com>
2022-11-26 15:44:35 -0500

    Close database connection on shutdown

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import util from 'util';
 import {WebSocketServer} from 'ws';
 
 import app from './app.js';
+import {db} from './db.js';
 import {handleConnection} from './driverManager.js';
 
 const port = process.env.PORT || 3000;
@@ -17,4 +18,5 @@ process.on('SIGTERM', shutdown);
 async function shutdown(signal: string) {
 	console.log(`Received ${signal}, shutting down`);
 	await util.promisify(httpServer.close.bind(httpServer))();
+	await db.destroy();
 }
